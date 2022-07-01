c4161817ac2485c1b6d1c37303a7128c818b5a6d
“00Amit0” <amit836961@gmail.com>
2022-07-02 03:41:38 +0530

    Creating Table

--- AddressBook_Query.sql
+++ AddressBook_Query.sql
@@ -2,3 +2,16 @@
 
 ---UC1--Creating data base---
 create database AddressBookService;
+USE AddressBookService;
+
+---UC2--Creating table-------
+CREATE TABLE addressbook(
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    zip INT NOT NULL,
+    phone_no VARCHAR(15) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+);
