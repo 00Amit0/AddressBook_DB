1172486cc8bc5397062ac8ee2709054d7331a23b
“00Amit0” <amit836961@gmail.com>
2022-07-02 03:44:21 +0530

    Inserting values

--- AddressBook_Query.sql
+++ AddressBook_Query.sql
@@ -15,3 +15,8 @@ CREATE TABLE addressbook(
     phone_no VARCHAR(15) NOT NULL,
     email VARCHAR(50) NOT NULL,
 );
+
+---UC3--Insert data----------
+INSERT INTO addressbook (first_name,last_name,address,city,state,zip,phone_no,email)
+    VALUES('Vladimir','Putin','3/40, Broadway Street','Moscow','Narnia',989001,'2354618524','v.putin@gmail.com'),
+        ('Sherlock','Holmes','Baker Street','London','England',567897,'7654874','sherlocked@gmail.com');
